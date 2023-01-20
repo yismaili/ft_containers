cc8a17f35856242056ffe5a99ea9a86cf287ed69
Younes Ismaili <yismaili@e2r12p5.1337.ma>
2023-01-20 19:48:56 +0100

    start operator overloading

--- Random_access_iterator.cpp
+++ Random_access_iterator.cpp
@@ -6,7 +6,7 @@
 /*   By: yismaili <yismaili@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 16:59:40 by yismaili          #+#    #+#             */
-/*   Updated: 2023/01/20 17:03:05 by yismaili         ###   ########.fr       */
+/*   Updated: 2023/01/20 18:04:23 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,24 @@
 namespace ft {      
     // template<typename T>
     // Random_access_iterator<T>::Random_access_iterator() {}
-    template <typename T>
-    Random_access_iterator<T>::Random_access_iterator(T *ptr_ = NULL)
-    {
-        this.ptr = ptr;
-    }
-    template <typename T>
-    Random_access_iterator<T>::~Random_access_iterator()
-    {
-    }
-     template <typename T>
-    Random_access_iterator<T>::Random_access_iterator(const Random_access_iterator &obj)
-    {
-        this.ptr = obj.ptr;
-    }
+    // template <typename T>
+    // Random_access_iterator<T>::Random_access_iterator(T *ptr_ = NULL)
+    // {
+    //     this.ptr = ptr;
+    // }
+    // template <typename T>
+    // Random_access_iterator<T>::~Random_access_iterator()
+    // {
+    // }
+    //  template <typename T>
+    // Random_access_iterator<T>::Random_access_iterator(const Random_access_iterator &obj)
+    // {
+    //     this.ptr = obj.ptr;
+    // }
+    // template <typename T>
+    // Random_access_iterator<T>::Random_access_iterator & Random_access_iterator:: operator=(const Random_access_iterator &obj)
+    // {
+    //     this->ptr = obj.ptr;
+    //     return (*this);
+    // }
 };
--- Random_access_iterator.hpp
+++ Random_access_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: yismaili <yismaili@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 15:38:03 by yismaili          #+#    #+#             */
-/*   Updated: 2023/01/20 17:18:03 by yismaili         ###   ########.fr       */
+/*   Updated: 2023/01/20 19:07:40 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,23 @@
    private:
         T *ptr
    public:
-    Random_access_iterator();
-    Random_access_iterator(T *ptr);
-    ~Random_access_iterator();
-     Random_access_iterator(const Random_access_iterator &obj)
-    Random_access_iterator()
-    {
-    }
+    Random_access_iterator(){}
     Random_access_iterator(T *ptr_)
     {
         this.ptr = ptr;
     }
-     ~Random_access_iterator()
+     ~Random_access_iterator(){}
+    Random_access_iterator(const Random_access_iterator &obj)
     {
+        this.ptr = obj.ptr;
     }
-   };
+    Random_access_iterator & operator=(const Random_access_iterator &obj)
+    {
+        this->ptr = obj.ptr;
+        return (*this);
+    }
+      std::ostream& operator == (std::ostream &) {
+       }  
+    };
  };
-
-
  
