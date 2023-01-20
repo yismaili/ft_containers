de525a7a056b4df6e921cb5f82be89a9a9181bb1
Younes Ismaili <yismaili@e2r12p5.1337.ma>
2023-01-20 17:08:29 +0100

    add constructors and destructor of  random access iterator

---
+++ Random_access_iterator.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Random_access_iterator.cpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yismaili <yismaili@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 16:59:40 by yismaili          #+#    #+#             */
+/*   Updated: 2023/01/20 17:03:05 by yismaili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Random_access_iterator.hpp"
+namespace ft {      
+    // template<typename T>
+    // Random_access_iterator<T>::Random_access_iterator() {}
+    template <typename T>
+    Random_access_iterator<T>::Random_access_iterator(T *ptr_ = NULL)
+    {
+        this.ptr = ptr;
+    }
+    template <typename T>
+    Random_access_iterator<T>::~Random_access_iterator()
+    {
+    }
+     template <typename T>
+    Random_access_iterator<T>::Random_access_iterator(const Random_access_iterator &obj)
+    {
+        this.ptr = obj.ptr;
+    }
+};
--- Random_access_iterator.hpp
+++ Random_access_iterator.hpp
@@ -6,16 +6,34 @@
 /*   By: yismaili <yismaili@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 15:38:03 by yismaili          #+#    #+#             */
-/*   Updated: 2023/01/19 15:39:52 by yismaili         ###   ########.fr       */
+/*   Updated: 2023/01/20 17:03:36 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-namespace ft {
-    class Random_access_iterator
-    {
-        private:
-            
-    }
-}
+ namespace ft {
+  template <typename T> class Random_access_iterator
+   {
+   private:
+        T *ptr
+   public:
+    Random_access_iterator();
+    Random_access_iterator(T *ptr);
+    ~Random_access_iterator();
+     Random_access_iterator(const Random_access_iterator &obj)
+//     Random_access_iterator()
+//     {
+//     }
+//     Random_access_iterator(T *ptr_)
+//     {
+//         this.ptr = ptr;
+//     }
+//      ~Random_access_iterator()
+//     {
+//     }
+   };
+ };
+
+
+ 
