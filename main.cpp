be1fce625169d83291dc58c29f8e0768e0225d9a
Younes Ismaili <yismaili@e2r12p5.1337.ma>
2023-01-22 17:51:21 +0100

    operator overlowding the prefix and  the postfix

--- main.cpp
+++ main.cpp
@@ -1,5 +1,35 @@
-#include <iostream>
-int main() {
- std:: cout << "Hello ft_containers" << std:: endl;
-  return 0;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yismaili <yismaili@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/22 13:50:25 by yismaili          #+#    #+#             */
+/*   Updated: 2023/01/22 17:38:07 by yismaili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<iostream>
+#include<iterator> 
+#include<vector>
+using namespace std;
+int main()
+{
+   vector<int> ar;
+ar.push_back(1);
+ar.push_back(2);
+ar.push_back(33);
+ar.push_back(4);
+ar.push_back(5);
+    vector<int>::iterator ptr;
+	ptr = ar.begin();
+    // cout << "The vector elements are : ";
+    // for(ptr = ar.begin(); ptr < ar.end(); ptr++)
+        cout << *ptr << " ";
+		*ptr = 10;
+		cout << *ptr << " ";
+      
+    return 0;    
 }
+
