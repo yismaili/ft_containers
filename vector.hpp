daf179c79179cb64d190883ba19b929c0ff515e7
Younes Ismaili <yismaili@e2r8p2.1337.ma>
2023-01-23 19:58:02 +0100

    define types

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: yismaili <yismaili@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 19:17:52 by yismaili          #+#    #+#             */
-/*   Updated: 2023/01/23 19:52:41 by yismaili         ###   ########.fr       */
+/*   Updated: 2023/01/23 19:56:40 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ public:
     typedef ft::Random_access_const_iterator<Random_access_iterator>    const_iterator;
     typedef ft::Random_access_reverse_iterator<Random_access_iterator>    reverse_iterator;
     typedef ft::Random_access_const_reverse_iterator<Random_access_iterator>    const_reverse_iterator;
+    typedef typename allocator_type::size_type       size_type;
+    typedef typename allocator_type::difference_type difference_type;
     vector(/* args */)
     {
     }
