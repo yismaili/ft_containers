15516cfd086a9cc3e8872b6c49af48888baef0f3
Younes Ismaili <yismaili@e2r7p2.1337.ma>
2023-01-24 14:01:22 +0100

    constructors and destructor of  vector

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: yismaili <yismaili@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 19:17:52 by yismaili          #+#    #+#             */
-/*   Updated: 2023/01/23 22:59:31 by yismaili         ###   ########.fr       */
+/*   Updated: 2023/01/24 14:01:09 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,35 +24,51 @@ public:
     typedef typename 	allocator_type::pointer pointer;
     typedef typename 	allocator_type::const_pointer const_pointer;
     typedef ft::Random_access_iterator<T>    iterator;
-    typedef ft::Random_access_const_iterator<T>    const_iterator;
+    typedef ft::Random_access_const_iterator<const T>    const_iterator;
     typedef ft::Random_access_reverse_iterator<T>    reverse_iterator;
     typedef ft::Random_access_const_reverse_iterator<T>    const_reverse_iterator;
     typedef typename allocator_type::difference_type difference_type;
     typedef typename allocator_type::size_type       size_type;
+    
     vector(const allocator_type & alloc = allocator_type){
         size = 0;
         ptr = NULL;
     }
+    
     vector (size_type n, const value_type& val = value_type(),const allocator_type& alloc = allocator_type()){
         size = n;
-        alloc.allocator(n);
-        for (size_t i = 0; i < n; i++)
-        {
-            
+        ptr = alloc.allocate(n);
+        for (size_t i = 0; i < n; i++){
+            alloc.construct(ptr + i, val);
         }
+        capacity = n;
         
     }
+    
     ~vector()
     {
     }
+    
+    template <class InputIterator>
+    vector (InputIterator first, InputIterator last,const allocator_type& alloc = allocator_type()){
+                    
+    }
+    
     vector& operator= (const vector& x)
     {
-       this.size = x.n;
-       this.alloc.allocator(x.n);
+        this.alloc.deallocate(ptr);
+        this.size = x.n;
+        this.ptr = this.alloc.allocate(x.n);
+         for (size_t i = 0; i < n; i++){
+            this.alloc.construct(this.ptr + i, x.val);
+        }
+        this.capacity = x.n;
     }
+    
 private:
     allocator_type alloc;
     size_type size;
     pointer ptr;
+    size_type capacity;
 };
 
