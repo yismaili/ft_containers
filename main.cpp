b7d7fa578726decda84a0bf9e611ae78c7d6ca3a
Younes Ismaili <yismaili@e2r7p1.1337.ma>
2023-02-02 17:02:11 +0100

    fix insert avec count (insert range to vector)

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: yismaili <yismaili@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 13:50:25 by yismaili          #+#    #+#             */
-/*   Updated: 2023/02/01 17:14:10 by yismaili         ###   ########.fr       */
+/*   Updated: 2023/02/02 16:55:08 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,18 +38,17 @@ int main ()
   for (int i=0;i < (int )myvector.size();i++)
     std::cout << ' ' << myvector[i];
   std::cout << '\n';
-// myvector.insert(myvector.end()+1 , 9);
-ft::vector<int>vctor(9);
-std::cout<< "size "<< vctor.size() << std::endl;
-std::cout<< "capasity "<< vctor.capacity() << std::endl;
-std::cout << "vctor contains:\n";
-for (int i=0;i < (int )vctor.size();i++)
-  std::cout << ' ' << vctor[i];
-std::cout << '\n';
-}
-catch(const std::exception& e){
-   std::cerr << e.what() << '\n';
-}
-
-  return 0;
+   myvector.insertt(myvector.cbegin(), 9, 2);
+ // ft::vector<int>vctor(myvector.begin(), myvector.end());
+  std::cout<< "size "<< myvector.size() << std::endl;
+  std::cout<< "capasity "<< myvector.capacity() << std::endl;
+  std::cout << "myvector contains:\n";
+  for (int i=0;i < (int )myvector.size();i++)
+    std::cout << ' ' << myvector[i];
+  std::cout << '\n';
+  }
+  catch(const std::exception& e){
+    std::cerr << e.what() << '\n';
+  }
+    return 0;
 }
