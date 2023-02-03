9b7700bdae3c87133352fd9dd7d0a5bc00324f0f
Younes Ismaili <yismaili@e2r5p4.1337.ma>
2023-02-03 19:52:06 +0100

    finish vector

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: yismaili <yismaili@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 13:50:25 by yismaili          #+#    #+#             */
-/*   Updated: 2023/02/03 19:32:00 by yismaili         ###   ########.fr       */
+/*   Updated: 2023/02/03 19:42:49 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,12 +40,10 @@ int main ()
   std::cout << '\n';
   //  myvector.insert(myvector.end(), myvector.begin() ,myvector.end());
  // ft::vector<int>vctor(myvector.begin(), myvector.end());
-  // std::cout << *myvector.rbegin()<<std::endl;
-  // std::cout << *myvector.rend()<<std::endl;
- myvector.begin()++;
-myvector.end()--;
-  std::cout <<*myvector.begin()<<std::endl;
-  std::cout <<*myvector.end()<<std::endl;
+  std::cout << *(myvector.rbegin() + 3)<<std::endl;
+  std::cout << *(myvector.rend() - 3)<<std::endl;
+  std::cout <<*(myvector.begin() + 3)<<std::endl;
+  std::cout <<*(myvector.end()-3)<<std::endl;
   // std::cout<< "size "<< myvector.size() << std::endl;
   // std::cout<< "capasity "<< myvector.capacity() << std::endl;
   // std::cout << "myvector contains:\n";
